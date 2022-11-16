f031cfdcfbd17c47db5569d557a551de4c0d08f6
EuHenrique <eusouzafirst@gmail.com>
2022-11-16 15:28:02 -0300

    fixed

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,19 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  name: string;
+  admin?: boolean;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  id?: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.admin = false;
+    }
+  }
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    console.log(user_id);
+    if (user) {
+      throw new Error("Usuario nao existe");
+    }
+
+    if (!user.admin) {
+      throw new Error("Usuario nao existe");
+    }
+    return this.usersRepository.list();
   }
 }
 
